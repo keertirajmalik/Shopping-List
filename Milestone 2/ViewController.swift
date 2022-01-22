8f2bb326f7529283f7bf2cb9e05e14cb861cf889
keertirajmalik <keertirajmalik@gmail.com>
2022-01-22 22:28:13 +0530

    Base Setup and add the both clear and add button clear -> Clean all the list add -> Add item to the list

--- Milestone 2/ViewController.swift
+++ Milestone 2/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var shoppingList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Shopping List"
         // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(updateList))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Clear",style: .done, target: self, action: #selector(clearList))
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "List", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+}
+extension ViewController {
+    
+    @objc func updateList(){
+       
+    }
+    
+    @objc func clearList(){
+       
+    }
+    
+   
 }
 
